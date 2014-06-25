81bdfa2786b1f0f2b85f4634b49896c5c6f38a88
Michael Vezzani <mjvezzani@gmail.com>
2014-06-24 20:54:28 -0400

    Simple rails app displaying 'Uninitialized Constant Bootsy' error after installing Booty.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit!)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+end
